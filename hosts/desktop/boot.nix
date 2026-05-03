01f631e5f7888ea922e51093ac5f20aed9a6a581
nyxier-512 <kyvd.god@gmail.com>
2026-05-03 07:44:43 +0200

    changed ip forwarding settings for tailscale

--- hosts/desktop/boot.nix
+++ hosts/desktop/boot.nix
@@ -8,9 +8,10 @@
       efi.canTouchEfiVariables = true;
     };
   };
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
+  # to use as tailscale exit node
+  ##boot.kernel.sysctl = {
+  ##  "net.ipv4.ip_forward" = 1;
+  ##  "net.ipv6.conf.all.forwarding" = 1;
+  ##};
 }
 
